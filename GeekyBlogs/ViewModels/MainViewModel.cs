714309b059ad900857253526756d348f6f6fdc13
dachibox <dachi.gogotchuri@gmail.com>
2015-09-05 19:06:49 +0200

    - refractoring

--- GeekyBlogs/ViewModels/MainViewModel.cs
+++ GeekyBlogs/ViewModels/MainViewModel.cs
@@ -19,16 +19,16 @@ namespace GeekyBlogs.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private readonly IFeedManagerService feedManagerService;
-        private readonly INavigationService navigationService;
         private readonly ISplitterMenuService splitterMenuService;
-        
-        private ObservableCollection<FeedItem> feeds;
-        private FeedItem feed;
 
-        public MainViewModel(IFeedManagerService feedManagerService, INavigationService navigationService, ISplitterMenuService splitterMenuService)
+        private ObservableCollection<FeedItem> feeds;
+        private List<FeedItem> allFeeds;
+        private FeedItem feed;
+        private string currentSizeState;
+
+        public MainViewModel(IFeedManagerService feedManagerService, ISplitterMenuService splitterMenuService)
         {
             this.feedManagerService = feedManagerService;
-            this.navigationService = navigationService;
             this.splitterMenuService = splitterMenuService;
 
             Feeds = new ObservableCollection<FeedItem>();
@@ -77,6 +77,7 @@ namespace GeekyBlogs.ViewModels
                                     AllFeeds.AddRange(tempList);
                                 }
                             }
+                            Feeds = tempList.ToObservableCollection();
                             break;
                     }
                     tempList.Sort((a, b) => b.PubDate.CompareTo(a.PubDate));
@@ -86,91 +87,12 @@ namespace GeekyBlogs.ViewModels
             }
         }
 
-        private double previousSize = 0;
-
         public override void AppView_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             base.AppView_SizeChanged(sender, e);
 
             PrepareGridViewForSize(Feeds.ToList());
-            previousSize = ViewWidth;
         }
-
-
-        private string currentSizeState;
-        public string CurrentSizeState
-        {
-            get { return currentSizeState; }
-            set
-            {
-                if (currentSizeState == value) return;
-                currentSizeState = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private void PrepareGridViewForSize(List<FeedItem> tempList)
-        {
-            if (tempList == null || tempList.Count == 0)
-                return;
-
-            if (CurrentSizeState != Enums.Size.OnehandState.ToString() && ViewWidth < (int)Enums.Size.OnehandState)
-            {
-                CurrentSizeState = Enums.Size.OnehandState.ToString();
-
-                tempList.ForEach(x =>
-                {
-                    x.RowSpan = 1; x.ColSpan = 2;
-                });
-                tempList[0].RowSpan = 1; tempList[0].ColSpan = 4;
-
-                Feeds = tempList.ToObservableCollection();
-            }
-            else if (CurrentSizeState != Enums.Size.MiddleState.ToString() 
-                && ViewWidth < (int)Enums.Size.MiddleState && ViewWidth > (int)Enums.Size.OnehandState)
-            {
-                CurrentSizeState = Enums.Size.MiddleState.ToString();
-
-                tempList.ForEach(x =>
-                {
-                    x.RowSpan = 1; x.ColSpan = 2;
-                });
-                tempList[0].RowSpan = 2; tempList[0].ColSpan = 2;
-                tempList[1].RowSpan = 2; tempList[1].ColSpan = 2;
-
-                Feeds = tempList.ToObservableCollection();
-            }
-            else if (CurrentSizeState != Enums.Size.DesktopState.ToString() 
-                && ViewWidth > (int)Enums.Size.MiddleState)
-            {
-                CurrentSizeState = Enums.Size.DesktopState.ToString();
-
-                tempList.ForEach(x =>
-                {
-                    x.RowSpan = 1; x.ColSpan = 1;
-                });
-                tempList[0].RowSpan = 2; tempList[0].ColSpan = 2;
-                tempList[1].RowSpan = 2; tempList[1].ColSpan = 2;
-
-                Feeds = tempList.ToObservableCollection();
-            }
-        }
-
-
-        private List<FeedItem> allFeeds;
-        public List<FeedItem> AllFeeds
-        {
-            get { return allFeeds; }
-            set
-            {
-                if (allFeeds != value)
-                {
-                    allFeeds = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
 
 
         public ObservableCollection<FeedItem> Feeds
@@ -196,6 +118,78 @@ namespace GeekyBlogs.ViewModels
                 OnPropertyChanged();
                 AppFrame.Navigate(typeof(ItemDetailView), Feed);
                 feed = null;
+            }
+        }
+
+        public List<FeedItem> AllFeeds
+        {
+            get { return allFeeds; }
+            set
+            {
+                if (allFeeds != value)
+                {
+                    allFeeds = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string CurrentSizeState
+        {
+            get { return currentSizeState; }
+            set
+            {
+                if (currentSizeState == value) return;
+                currentSizeState = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void PrepareGridViewForSize(List<FeedItem> tempList)
+        {
+            if (tempList == null || tempList.Count == 0)
+                return;
+
+            if (CurrentSizeState != Enums.Size.OnehandState.ToString() && ViewWidth < (int)Enums.Size.OnehandState)
+            {
+                CurrentSizeState = Enums.Size.OnehandState.ToString();
+
+                tempList.ForEach(x =>
+                {
+                    x.RowSpan = 1; x.ColSpan = 2;
+                });
+                tempList[0].RowSpan = 1; tempList[0].ColSpan = 4;
+
+                Feeds = tempList.ToObservableCollection();
+            }
+            else if (CurrentSizeState != Enums.Size.MiddleState.ToString() 
+                     && ViewWidth < (int)Enums.Size.MiddleState && ViewWidth > (int)Enums.Size.OnehandState)
+            {
+                CurrentSizeState = Enums.Size.MiddleState.ToString();
+
+                tempList.ForEach(x =>
+                {
+                    x.RowSpan = 1; x.ColSpan = 2;
+                });
+                tempList[0].RowSpan = 2; tempList[0].ColSpan = 2;
+                tempList[1].RowSpan = 2; tempList[1].ColSpan = 2;
+
+                Feeds = tempList.ToObservableCollection();
+            }
+            else if (CurrentSizeState != Enums.Size.DesktopState.ToString() 
+                     && ViewWidth > (int)Enums.Size.MiddleState)
+            {
+                CurrentSizeState = Enums.Size.DesktopState.ToString();
+
+                tempList.ForEach(x =>
+                {
+                    x.RowSpan = 1; x.ColSpan = 1;
+                });
+                tempList[0].RowSpan = 2; tempList[0].ColSpan = 2;
+                tempList[1].RowSpan = 2; tempList[1].ColSpan = 2;
+
+                Feeds = tempList.ToObservableCollection();
             }
         }
     }
