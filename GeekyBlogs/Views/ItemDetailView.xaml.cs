d4799d3112fa7a91b3ae4c42e94d11b7cc030b48
dachibox <dachi.gogotchuri@gmail.com>
2015-09-07 00:33:59 +0200

    - DisableScrollingJs just not working

--- GeekyBlogs/Views/ItemDetailView.xaml.cs
+++ GeekyBlogs/Views/ItemDetailView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using GeekyBlogs.ViewModels;
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using GeekyBlogs.ViewModels;
 using GeekyTool;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -15,6 +18,31 @@ namespace GeekyBlogs.Views
             this.InitializeComponent();
 
             this.SizeChanged += ((ItemDetailViewModel)this.DataContext).AppView_SizeChanged;
+        }
+
+        private const string DisableScrollingJs = @"function RemoveScrolling()
+                              {
+                                  var styleElement = document.createElement('style');
+                                  var styleText = 'body, html { overflow: hidden; }'
+                                  var headElements = document.getElementsByTagName('head');
+                                  styleElement.type = 'text/css';
+                                  if (headElements.length == 1)
+                                  {
+                                      headElements[0].appendChild(styleElement);
+                                  }
+                                  else if (document.head)
+                                  {
+                                      document.head.appendChild(styleElement);
+                                  }
+                                  if (styleElement.styleSheet)
+                                  {
+                                      styleElement.styleSheet.cssText = styleText;
+                                  }
+                              }";
+
+        private void WebView_OnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            webView.InvokeScriptAsync("eval", new[] { DisableScrollingJs });
         }
     }
 }
