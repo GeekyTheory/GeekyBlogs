a1006dff887a841366710b46f89a6cb86392708a
Gabriel Ferreiro <gbril9119@gmail.com>
2015-09-09 23:51:44 +0200

    Added boolean control to suggestbox

--- GeekyBlogs/Controls/AdaptiveSuggestBox.xaml.cs
+++ GeekyBlogs/Controls/AdaptiveSuggestBox.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -6,9 +9,37 @@ namespace GeekyBlogs.Controls
 {
     public sealed partial class AdaptiveSuggestBox : UserControl
     {
+        private bool isOpened;
+
         public AdaptiveSuggestBox()
         {
             this.InitializeComponent();
+            Window.Current.SizeChanged += CurrentWindowOnSizeChanged;
+        }
+
+        private void CurrentWindowOnSizeChanged(object sender, WindowSizeChangedEventArgs windowSizeChangedEventArgs)
+        {
+            if (windowSizeChangedEventArgs.Size.Width <= 992)
+            {
+                isOpened = false;
+            }
+            else
+            {
+                isOpened = true;
+            }
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (Window.Current.Bounds.Width <= 992)
+            {
+                VisualStateManager.GoToState(this, !isOpened ? "LargeDevices" : "MediumDevices", true);
+                isOpened = !isOpened;
+            }
+            else
+            {
+                isOpened = false;
+            }
         }
     }
 }
