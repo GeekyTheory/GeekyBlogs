56f91d4ff5a2b8454c4d19a337a82c4b6be97bc9
Gabriel Ferreiro <gbril9119@gmail.com>
2015-09-09 21:52:11 +0200

    Added AdaptiveSuggestBox control

---
+++ GeekyBlogs/Controls/AdaptiveSuggestBox.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace GeekyBlogs.Controls
+{
+    public sealed partial class AdaptiveSuggestBox : UserControl
+    {
+        public AdaptiveSuggestBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
