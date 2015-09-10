b434ed10cd15a0d252e853d1e65044917d990706
dachibox <dachi.gogotchuri@gmail.com>
2015-09-10 23:50:30 +0200

    little fixes

--- GeekyBlogs/Views/ItemDetailView.xaml.cs
+++ GeekyBlogs/Views/ItemDetailView.xaml.cs
@@ -19,30 +19,5 @@ namespace GeekyBlogs.Views
 
             this.SizeChanged += ((ItemDetailViewModel)this.DataContext).AppView_SizeChanged;
         }
-
-        private const string DisableScrollingJs = @"function RemoveScrolling()
-                              {
-                                  var styleElement = document.createElement('style');
-                                  var styleText = 'body, html { overflow: hidden; }'
-                                  var headElements = document.getElementsByTagName('head');
-                                  styleElement.type = 'text/css';
-                                  if (headElements.length == 1)
-                                  {
-                                      headElements[0].appendChild(styleElement);
-                                  }
-                                  else if (document.head)
-                                  {
-                                      document.head.appendChild(styleElement);
-                                  }
-                                  if (styleElement.styleSheet)
-                                  {
-                                      styleElement.styleSheet.cssText = styleText;
-                                  }
-                              }";
-
-        private void WebView_OnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
-        {
-            webView.InvokeScriptAsync("eval", new[] { DisableScrollingJs });
-        }
     }
 }
